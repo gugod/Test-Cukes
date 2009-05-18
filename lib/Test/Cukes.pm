45acea345cc7458c290a8ababcd8084967b33277
Kang-min Liu <gugod@gugod.org>
2009-05-19 02:55:24 +0800

    initial commit

---
+++ lib/Test/Cukes.pm
@@ -0,0 +1,33 @@
+package Test::Cukes;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Test::Cukes -
+
+=head1 SYNOPSIS
+
+  use Test::Cukes;
+
+=head1 DESCRIPTION
+
+Test::Cukes is
+
+=head1 AUTHOR
+
+Kang-min Liu E<lt>gugod@gugod.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
