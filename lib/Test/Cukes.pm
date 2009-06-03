5bf11a232037a7a97a1d082856815c05071d86dd
Kang-min Liu <gugod@gugod.org>
2009-06-03 16:42:30 +0800

    let each scenario step be a block of assertion-based test.
    then we can finally get rid of the process of counting "ok"s.

--- lib/Test/Cukes.pm
+++ lib/Test/Cukes.pm
@@ -5,7 +5,7 @@ use Exporter::Lite;
 use Test::More;
 use Test::Cukes::Feature;
 
-our $VERSION = "0.01";
+our $VERSION = "0.02";
 our @EXPORT = qw(feature runtests Given When Then);
 
 my $steps = {};
@@ -20,20 +20,39 @@ sub feature {
 
 sub runtests {
     my $caller = caller;
+    my $total_tests = 0;
+    for my $scenario (@{$feature->{$caller}->scenarios}) {
+        $total_tests += @{$scenario->steps};
+    }
+
+    Test::More::plan(tests => $total_tests);
+
+    my $skip = 0;
+    my $skip_reason = "";
     for my $scenario (@{$feature->{$caller}->scenarios}) {
         my %steps = %{$steps->{$caller}};
         for my $step_text (@{$scenario->steps}) {
-            Test::More::note( $step_text );
-
             my (undef, $step) = split " ", $step_text, 2;
+
+        SKIP:
             while (my ($step_pattern, $cb) = each %steps) {
+                Test::More::skip($step, 1) if $skip;
+
                 if ($step =~ m/$step_pattern/) {
-                    $cb->();
+                    eval { $cb->(); };
+                    Test::More::ok(!$@, $step_text);
+
+                    if ($@) {
+                        $skip = 1;
+                        $skip_reason = "Failed: $step_text";
+                    }
                     next;
                 }
             }
         }
     }
+
+    return 0;
 }
 
 sub _add_step {
