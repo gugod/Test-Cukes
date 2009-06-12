25125a86aecdb6ea1cd8b9205f498b8af55293d5
Liu Kang Min <gugod@gugod.org>
2009-06-12 01:48:53 -0800

    import Test-Cukes 0.04 from CPAN
    git-cpan-module:   Test-Cukes
    git-cpan-version:  0.04
    git-cpan-authorid: GUGOD
    git-cpan-file:     authors/id/G/GU/GUGOD/Test-Cukes-0.04.tar.gz

--- lib/Test/Cukes.pm
+++ lib/Test/Cukes.pm
@@ -6,8 +6,10 @@ use Test::More;
 use Test::Cukes::Feature;
 use Carp::Assert;
 
-our $VERSION = "0.02";
+our $VERSION = "0.04";
 our @EXPORT = qw(feature runtests Given When Then assert affirm should shouldnt);
+
+our @missing_steps = ();
 
 my $steps = {};
 my $feature = {};
@@ -22,38 +24,59 @@ sub feature {
 sub runtests {
     my $caller = caller;
     my $total_tests = 0;
-    for my $scenario (@{$feature->{$caller}->scenarios}) {
+    my @scenarios_of_caller = @{$feature->{$caller}->scenarios};
+
+    for my $scenario (@scenarios_of_caller) {
         $total_tests += @{$scenario->steps};
     }
 
     Test::More::plan(tests => $total_tests);
 
-    my $skip = 0;
-    my $skip_reason = "";
-    for my $scenario (@{$feature->{$caller}->scenarios}) {
-        my %steps = %{$steps->{$caller}};
+    for my $scenario (@scenarios_of_caller) {
+        my $skip = 0;
+        my $skip_reason = "";
+
+        my %steps = %{$steps->{$caller} ||{}};
+    SKIP:
         for my $step_text (@{$scenario->steps}) {
             my (undef, $step) = split " ", $step_text, 2;
+            Test::More::skip($step, 1) if $skip;
 
-        SKIP:
+            my $found_step = 0;
             while (my ($step_pattern, $cb) = each %steps) {
-                Test::More::skip($step, 1) if $skip;
-
                 if ($step =~ m/$step_pattern/) {
                     eval { $cb->(); };
                     Test::More::ok(!$@, $step_text);
 
                     if ($@) {
+                        Test::More::diag($@);
                         $skip = 1;
                         $skip_reason = "Failed: $step_text";
                     }
-                    next;
+
+                    $found_step = 1;
+                    last;
                 }
+            }
+
+            unless($found_step) {
+                push @missing_steps, $step_text;
             }
         }
     }
 
+    report_missing_steps();
+
     return 0;
+}
+
+sub report_missing_steps {
+    Test::More::note("There are missing step definitions, fill them in:");
+    for my $step_text (@missing_steps) {
+        my ($word, $text) = ($step_text =~ /^(Given|When|Then) (.+)$/);
+        my $msg = "\n$word qr/${text}/ => sub {\n    ...\n}\n";
+        Test::More::note($msg);
+    }
 }
 
 sub _add_step {
